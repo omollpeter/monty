ca31ae6c95b51a20dda3512e799ea64cea2d07e2
Nickhongo <hongonicholas0@gmail.com>
2024-01-18 12:23:45 +0300

    Create a function that prints a character at the top of a stack

--- pchar.c
+++ pchar.c
@@ -1,1 +1,25 @@
 #include "monty.h"
+
+/**
+ * pchar - Prints the char at the top of the stack/queue
+ * @stack: Pointer to pointer to the top of the stack
+ * @line_number: Line number in the monty bytecode file
+ *
+ * Return: nothing
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(stdout, "%c\n", (*stack)->n);
+}
